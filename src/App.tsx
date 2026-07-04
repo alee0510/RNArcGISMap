f980d9772b3a2d450ef02394c47f554b336cde44
Ali Muksin <ali.muksin0510@gmail.com>
2026-07-04 18:44:09 +0700

    feat: implement new NativeLocationModule

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import { useEffect } from 'react'
+import { StatusBar, StyleSheet, View, Text } from 'react-native'
+import { useColorScheme } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import LocationModule, { LocationPermission } from "@/native/NativeLocationModule";
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const init = async () => {
+    const permission = await LocationModule.checkLocationPermission()
+    if (permission === LocationPermission.DENIED) {
+      const result = await LocationModule.requestLocationPermission()
+      if (result === LocationPermission.DENIED) {
+        console.log("ERROR: permission denied!")
+        return
+      }
+    }
+    const location = await LocationModule.getCurrentLocation()
+    console.log("INFO: location", location)
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
 
   return (
     <SafeAreaProvider>

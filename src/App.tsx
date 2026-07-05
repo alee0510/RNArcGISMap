bf816743be3fb07c447d77e686a1e6a9efc83952
Ali Muksin <ali.muksin0510@gmail.com>
2026-07-05 14:55:30 +0700

    feat: implement arcgis map view on the rn side

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react'
-import { StatusBar, StyleSheet, View, Text } from 'react-native'
-import { useColorScheme } from 'react-native'
+import { useEffect } from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import LocationModule, { LocationPermission } from "@/native/NativeLocationModule";
+import ArcGISMapView from "@/native/NativeArcGISMapViewNativeComponent";
+import ErrorBoundaryWrapper from '@/utils/ErrorBoundary';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -25,12 +27,19 @@ function App() {
   }, [])
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={styles.container}>
-          <Text>Hello</Text>
-      </View>
-    </SafeAreaProvider>
+    <ErrorBoundaryWrapper>
+      <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <View style={styles.container}>
+          <ArcGISMapView
+            style={styles.map}
+            latitude={34.0270}
+            longitude={-118.8050}
+            onMapTap={(e) => console.log(e.nativeEvent.latitude, e.nativeEvent.longitude)}
+          />
+        </View>
+      </SafeAreaProvider>
+    </ErrorBoundaryWrapper>
   );
 }
 
@@ -38,8 +47,15 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-      backgroundColor: '#fff',
+    backgroundColor: '#fff',
   },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
 });
 
 export default App;

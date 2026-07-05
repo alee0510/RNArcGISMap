92a46d2554b6e15d692b6873f90d57b7e2518411
Ali Muksin <ali.muksin0510@gmail.com>
2026-07-05 14:51:43 +0700

    refactor: rename the package and implement new module and native view component

--- android/app/src/main/java/com/rnarcgis/MyPackage.kt
+++ android/app/src/main/java/com/rnarcgis/MyPackage.kt
@@ -5,11 +5,13 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.facebook.react.uimanager.ViewManager
 
-class LocationPackage : BaseReactPackage() {
+class MyPackage : BaseReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
         return when (name) {
             LocationModule.NAME -> LocationModule(reactContext)
+            ArcGISMapModule.NAME -> ArcGISMapModule(reactContext)
             else -> null
         }
     }
@@ -20,8 +22,16 @@ class LocationPackage : BaseReactPackage() {
                 LocationModule.NAME to ReactModuleInfo(
                     LocationModule.NAME, LocationModule.NAME,
                     false, false, false, true
+                ),
+                ArcGISMapModule.NAME to ReactModuleInfo(
+                    ArcGISMapModule.NAME, ArcGISMapModule.NAME,
+                    false, false, false, true
                 )
             )
         }
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<in Nothing, in Nothing>> {
+        return listOf(ArcGISMapViewManager())
     }
 }

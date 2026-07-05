f2c95583f700f1dfc9533f5c3b091bbe18f9644d
Ali Muksin <ali.muksin0510@gmail.com>
2026-07-05 14:51:06 +0700

    update: arcgis map native component props specs

---
+++ src/native/NativeArcGISMapViewNativeComponent.ts
@@ -0,0 +1,19 @@
+import { codegenNativeComponent } from 'react-native';
+import type { HostComponent, ViewProps } from 'react-native';
+import type { Double, DirectEventHandler } from 'react-native/Libraries/Types/CodegenTypes';
+
+interface MapTapEven {
+    latitude: Double,
+    longitude: Double,
+}
+
+export interface NativeProps extends ViewProps {
+    longitude?: Double;
+    latitude?: Double;
+    zoomScale?: Double;
+    pinsJson?: string; // serialized array of {lat, lng, id}
+    routeGeometryJson?: string; // serialized polyline from the routing TurboModule
+    onMapTap?: DirectEventHandler<MapTapEven>;
+}
+
+export default codegenNativeComponent<NativeProps>("ArcGISMapView") as HostComponent<NativeProps>;

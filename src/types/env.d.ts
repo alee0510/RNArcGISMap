ce28de521f5fe86a0c0c2f4fce790ec5b62322d2
Ali Muksin <ali.muksin0510@gmail.com>
2026-07-06 10:48:42 +0700

    feat: implement environment variable configuration and validation using zod

---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+    export const ARCGISMAP_KEY: string;
+    export const ARCGIS_GEOCODE_SUGGEST_URL: string;
+    export const ARCGIS_GEOCODE_FIND_CANDIDATE: string;
+    export const ARCGIS_GEOCODE_REVERSE_CODE: string;
+}

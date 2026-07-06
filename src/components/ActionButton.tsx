c83999349ffb98f80281d3866112ce3902399fd1
Ali Muksin <ali.muksin0510@gmail.com>
2026-07-07 00:24:18 +0700

    refactor: introduce reusable ActionButton component and implement BottonNavBar in HomeScreen

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, View, ViewStyle } from "react-native"
+import { Icon, MD3Colors } from "react-native-paper"
+
+interface ActionButtonProps {
+    icon: string,
+    iconSize?: number,
+    space?: number,
+    activeIconColor?: string,
+    inactiveIconColor?: string,
+    activeBackgroundColor?: string,
+    inactiveBackgroundColor?: string,
+    borderColor?: string,
+    border?: number,
+    active?: boolean,
+    style?: ViewStyle
+}
+
+export default function ActionButton({
+    icon,
+    iconSize = 24,
+    space = 10,
+    activeIconColor = MD3Colors.primary40,
+    inactiveIconColor = MD3Colors.secondary70,
+    activeBackgroundColor = MD3Colors.primary100,
+    inactiveBackgroundColor = MD3Colors.primary100,
+    borderColor = MD3Colors.primary40,
+    border = 0,
+    active = false,
+    style
+}: ActionButtonProps) {
+    return (
+        <View style={[
+            styles.container,
+            {
+                backgroundColor: active ? activeBackgroundColor : inactiveBackgroundColor,
+                borderColor: borderColor,
+                borderWidth: border,
+                padding: space,
+            },
+            style
+        ]}>
+            <Icon source={icon} size={iconSize} color={active ? activeIconColor : inactiveIconColor} />
+        </View >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 50
+    }
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,10 @@
-import { Dimensions, StyleSheet, View } from 'react-native';
+import { useLocationStore } from "@/hooks/useLocation.ts";
 import { Text, Icon, MD3Colors } from 'react-native-paper';
-import { useLocationStore } from "@/hooks/useLocation.ts"
-import Map from "@/components/Map.tsx"
+import { Dimensions, StyleSheet, View } from 'react-native';
+import ActionButton from "@/components/ActionButton.tsx";
+import ButtonNavBar from "@/components/BottonNavBar.tsx";
+import Map from "@/components/Map.tsx";
+
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 export default function HomeScreen() {
@@ -10,42 +13,23 @@ export default function HomeScreen() {
         <View style={styles.container}>
             <Map />
             <View style={styles.header}>
-                <View style={styles.location}>
+                <View style={styles.locationContainer}>
                     <View style={styles.locationIcon}>
                         <Icon source="map-marker-outline" size={24} color={MD3Colors.secondary70} />
                     </View>
                     <Text variant="titleMedium" style={styles.locationText}>{`${address.City}, ${address.CntryName}`}</Text>
                 </View>
-                <View style={[styles.actionIcon, styles.actionIconWithBorder]}>
-                    <Icon source="layers-outline" size={24} color={MD3Colors.secondary70} />
-                </View>
+                <ActionButton icon="layers-outline" border={1} />
             </View>
-            <View style={styles.bottomNavBar}>
-                <View style={[styles.bottomNavBarIcon, styles.bottomNavBarIconActive]}>
-                    <Icon source="map" size={24} color={MD3Colors.primary40} />
-                </View>
-                <View style={styles.bottomNavBarIcon}>
-                    <Icon source="pin" size={24} color={MD3Colors.primary80} />
-                </View>
-                <View style={styles.bottomNavBarIcon}>
-                    <Icon source="routes" size={24} color={MD3Colors.primary80} />
-                </View>
-            </View>
-            <View style={[styles.actionIcon, styles.actionIconWithBorder, styles.recenter]}>
-                <Icon source="crosshairs" size={24} color={MD3Colors.secondary70} />
-            </View>
+            <ButtonNavBar />
+            <ActionButton icon="crosshairs" border={1} style={styles.recenter} />
             <View style={styles.zoomAction}>
-                <View style={[styles.actionIcon, styles.actionIconWithBorder]}>
-                    <Icon source="plus" size={24} color={MD3Colors.secondary70} />
-                </View>
-                <View style={[styles.actionIcon, styles.actionIconWithBorder]}>
-                    <Icon source="minus" size={24} color={MD3Colors.secondary70} />
-                </View>
+                <ActionButton icon="plus" border={1} />
+                <ActionButton icon="minus" border={1} />
             </View>
         </View>
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
@@ -61,7 +45,7 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
     },
-    location: {
+    locationContainer: {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
@@ -78,15 +62,6 @@ const styles = StyleSheet.create({
         paddingRight: 15,
         paddingVertical: 5,
     },
-    actionIcon: {
-        backgroundColor: MD3Colors.primary100,
-        padding: 10,
-        borderRadius: 50,
-    },
-    actionIconWithBorder: {
-        borderWidth: 1,
-        borderColor: MD3Colors.primary40
-    },
     recenter: {
         position: "absolute",
         bottom: 150,
@@ -99,26 +74,5 @@ const styles = StyleSheet.create({
         gap: 10,
         bottom: SCREEN_HEIGHT / 2,
         right: 20,
-    },
-    bottomNavBar: {
-        backgroundColor: MD3Colors.primary40,
-        borderRadius: 50,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        padding: 4,
-        marginHorizontal: "25%",
-        marginVertical: 20,
-        position: "absolute",
-        bottom: 20,
-        width: "50%",
-    },
-    bottomNavBarIcon: {
-        borderRadius: 50,
-        padding: 15
-    },
-    bottomNavBarIconActive: {
-        backgroundColor: MD3Colors.primary80,
-        borderRadius: 50,
     }
 });
